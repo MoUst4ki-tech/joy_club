66255d396c035f1733d6b0e2266f2ed74c2e271b
MoUst4ki-tech <noe1.marchal@epitech.eu>
2026-04-25 13:36:56 +0200

    feat: add script logique

---
+++ script.js
@@ -0,0 +1,16 @@
+let userBalance = 100;
+
+function updateDisplay() {
+    const display = document.getElementById('gold-eggs-display');
+    if(display) {
+        display.innerText = userBalance;
+    }
+}
+
+function updateBalance(amount) {
+    userBalance += amount;
+    
+    if (userBalance < 0) userBalance = 0;
+    updateDisplay();
+    console.log("Nouveau solde : " + userBalance);
+}
